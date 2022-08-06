c7227a982b3a6ac2fb0c72328740f99bf1c0b18f
tiddster <864187149@qq.com>
2022-08-06 16:02:54 +0800

    批量归一化

---
+++ 5. CNN/BatchNorm.py
@@ -0,0 +1,87 @@
+import torch
+from torch import nn,optim
+import torch.nn.functional as F
+
+import DIDLutils
+import LoadFashionMNIST
+
+"""
+批量归一化层：
+任意一个特征在数据集中所有样本上的均值为0、标准差为1。标准化处理输入数据使各个特征的分布相近：这往往更容易训练出有效的模型。
+"""
+"""
+自实现
+"""
+def batch_norm(is_training, X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    # 判断当前模式是训练模式还是预测模式
+    if not is_training:
+        # 如果是在预测模式下，直接使用传入的移动平均所得的均值和方差
+        X_hat = (X-moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        assert len(X.shape) in (2,4)
+        if len(X.shape) == 2:
+            mean = X.mean(dim=0)
+            var = ((X-mean)**2).mean(dim=0)
+        else:
+            # 使用二维卷积层的情况，计算通道维上（axis=1）的均值和方差。这里我们需要保持
+            # X的形状以便后面可以做广播运算
+            mean = X.mean(dim=0, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
+            var = ((X - mean) ** 2).mean(dim=0, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
+            # 训练模式下用当前的均值和方差做标准化
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        # 更新移动平均的均值和方差
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # 拉伸和偏移
+    return Y, moving_mean, moving_var
+
+class BatchNorm(nn.Module):
+    def __init__(self, num_feature, num_dims):
+        super(BatchNorm, self).__init__()
+        if num_dims == 2:
+            shape = (1, num_feature)
+        else:
+            shape = (1, num_feature, 1, 1)
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.zeros(shape)
+
+    def forward(self, X):
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        Y, self.moving_mean, self.moving_var = batch_norm(self.training,
+            X, self.gamma, self.beta, self.moving_mean,
+            self.moving_var, eps=1e-5, momentum=0.9)
+        return Y
+
+"""
+自带归一函数
+"""
+
+net = nn.Sequential(
+nn.Conv2d(1, 6, 5), # in_channels, out_channels, kernel_size
+            nn.BatchNorm2d(6),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2), # kernel_size, stride
+            nn.Conv2d(6, 16, 5),
+            nn.BatchNorm2d(16),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2),
+            DIDLutils.FlattenLayer(),
+            nn.Linear(16*4*4, 120),
+            nn.BatchNorm1d(120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.BatchNorm1d(84),
+            nn.Sigmoid(),
+            nn.Linear(84, 10)
+)
+if __name__ == '__main__':
+    batch_size = 256
+    train_iter, test_iter = LoadFashionMNIST.load_data_fashion_mnist(batch_size=batch_size)
+
+    lr, num_epochs = 0.001, 5
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    DIDLutils.train_CNNet(net, train_iter, test_iter, batch_size, optimizer, DIDLutils.device, num_epochs)
