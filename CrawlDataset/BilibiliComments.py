998f1861db9adcd8a95def359791640124886f30
tiddster <864187149@qq.com>
2022-09-18 20:44:03 +0800

    bilibili 评论

--- CrawlDataset/BilibiliComments.py
+++ CrawlDataset/BilibiliComments.py
@@ -1,69 +1,28 @@
-import re
 import requests
-from selenium import webdriver
-from requests.exceptions import RequestException
 import time
 
-# 获取评论第一页的源码
-def get_first_page_source(url,content_list):
-    browser = webdriver.Chrome()
-    browser.get(url)
-    browser.execute_script('window.scrollBy(0,2000)')#调用js代码实现滑动
-    time.sleep(4)#等待加载完毕
-    content_list.append(browser.page_source)
-    return browser.page_source
+save_path = "F:\Dataset\Bilibili\\"
 
-# 获取评论总页数
-def get_max_size_page_info(html):
-    pattern = re.compile('<div.*?header-page.*?result">共(\d+)页</span>',re.S)
-    return re.findall(pattern,html)[0]
+page = 0
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39'
+}
 
-# 获取其他页的源码
-def get_more_page_source(url,content_list):
-    browser = webdriver.Chrome()
-    # browser.set_window_size(1000,30000)
-    browser.get(url)
-    browser.execute_script('window.scrollBy(0,2000)')#调用js代码实现滑动
-    time.sleep(4)#等待加载完毕
-    content_list.append(browser.page_source)
-    max_size = int(get_max_size_page_info(get_first_page_source(url,content_list)))
-    for i in range(max_size-1):
-        next_btn = browser.find_element_by_css_selector('div.comment-header div.header-page a.next')#使用css选择器选择下一页a标签
-
-        next_btn.click()
-        time.sleep(1.5)
-        content_list.append(browser.page_source)
+with open(save_path + "math.txt", 'w') as f:
+    while True:
+        print(f"==============={page}==================")
+        url = f"https://api.bilibili.com/x/v2/reply/main?csrf=05e21359595373aab5f2d891d9ab431d&mode=3&next={page}&oid=471900826&plat=1&type=1"
+        response = requests.get(url, headers=headers, verify=False)
+        response.encoding = 'utf-8'
+        Json_reply_list = response.json()['data']['replies']
+        if Json_reply_list:
+            for i in range(len(Json_reply_list)):
+                comment = Json_reply_list[i]["content"]["message"]
+                f.write(comment + "\n")
+                print(comment)
+        else:
+            print("======================over=====================")
+            break
+        page += 1
 
 
-# 获取评论信息
-def get_comment(content):
-    # 使用正则表达式匹配
-    pattern = re.compile('<div.*?list-item.*?data-id="(\d+)".*?con.*?">.*?data-usercard-mid="(\d+)".*?name.*?">'
-                             + '(.*?)</a>.*?<p.*?text">(.*?)</p>.*?</div>.*?</div>',re.S)
-    for item in content:
-        info = re.findall(pattern,item)
-        for info_item in info:
-            # 使用生成器规范化数据
-            yield {
-                'comment_id':info_item[0],
-                'user_id':info_item[1],
-                'user_name':info_item[2],
-                'comment_text':info_item[3]
-            }
-    # return info
-
-
-def main():
-    url = 'https://www.bilibili.com/video/BV1Eb411u7Fw?spm_id_from=333.337.search-card.all.click&vd_source=44ed1dfa2df376ac8b769e49d44e6267'
-    comment_list = []
-    get_more_page_source(url,comment_list)
-    get_comment(comment_list)
-
-    with open('D://comment_list.txt','a',encoding='utf-8') as f:
-        for item in get_comment(comment_list):
-            f.write(str(item) + '\n')
-    print(len(comment_list))
-
-
-if __name__ == '__main__':
-    main()
