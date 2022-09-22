b78979bf346f03096458bdc5f18e6f8617464104
tiddster <864187149@qq.com>
2022-09-22 21:06:50 +0800

    情感分析

--- CrawlDataset/BaiduSentiment.py
+++ CrawlDataset/BaiduSentiment.py
@@ -1,14 +1,16 @@
 import time
 
 from aip import AipNlp
+import pandas as pd
 import json
 import BilibiliComments
+from threading import Lock
 
 save_path = BilibiliComments.save_path
 save_txts = BilibiliComments.save_txts
 # 百度情感分析api
 api_path = "F:\Dataset\\baidu_api.txt"
-root_path = "F:\Dataset\\Bilibili"
+root_path = "F:\Dataset\\Bilibili\\"
 
 file = open(api_path, 'r')
 js = file.read()
@@ -29,66 +31,78 @@ textDic = {"AM": [], "LA": [], "PS": [], "CN": [], "COS": [], "CC": []}
 for (title, _), txt in zip(textDic.items(), save_txts):
     textDic[title] = load_data(txt)
 
-AMDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
-LADict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
-PSDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
-CNDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
-COSDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
-CCDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# AMDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# LADict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# PSDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# CNDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# COSDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
+# CCDict = {"pos": [], "neg": [], "neu": [], "low_confidence": []}
 
 client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
+csvTextList = []
+lock = Lock()
 
-def classifyDict(dict, textList):
-    for text in textList:
-        if len(text) == 0: continue
+def classifyText(text):
+    if text is not None:
+        if len(text) == 0: return
         text = text.encode('gbk', errors='ignore').decode('gbk').encode('utf-8').decode('utf-8')
         res = client.sentimentClassify(text)
-        time.sleep(1)
+        time.sleep(0.1)
         # res = {'text': '后来，我们还是上了同一所大学[doge]', 'items': [{'confidence': 0.88032, 'negative_prob': 0.0538557, 'positive_prob': 0.946144, 'sentiment': 2}], 'log_id': 1572567722348422346}
         if res.get("items") != None:
-            print(res)
+            # print(res)
             itemsDict = res['items'][0]
             confidence = itemsDict['confidence']
             neg_prob = itemsDict['negative_prob']
             pos_prob = itemsDict['positive_prob']
 
             if confidence < 0.5:
-                dict["low_confidence"].append(text)
-                if 0.4 <= pos_prob <= 0.6:
-                    dict["neu"].append(text)
-            elif pos_prob >= 0.5:
-                dict["pos"].append(text)
+                csvTextList.append(["中性", text])
+            elif pos_prob > 0.5:
+                csvTextList.append(["积极", text])
             elif neg_prob > 0.5:
-                dict["pos"].append(text)
+                csvTextList.append(["消极", text])
 
 
-classifyDict(AMDict, textDic["AM"])
-print(1)
-classifyDict(LADict, textDic["LA"])
-print(1)
-classifyDict(PSDict, textDic["PS"])
-print(1)
-classifyDict(CNDict, textDic["CN"])
-print(1)
-classifyDict(COSDict, textDic["COS"])
-print(1)
-classifyDict(CCDict, textDic["CC"])
+textList = textDic["AM"]
+csvName = "AdvanceMath.csv"
+
+i = 0
+for text in textList:
+    classifyText(text)
+    i += 1
+    if i % 100 == 0:
+        print(len(csvTextList))
+
+idx = pd.date_range(start=1, periods=len(csvTextList))
+csvData = pd.DataFrame(csvTextList, columns=["情感", "文本"])
+csvData.to_csv(root_path + csvName, encoding='utf-8')
 
 
-def save_dict(dict, filename):
-    js = json.dumps(dict)
-    file = open(root_path + filename, 'w')
-    file.write(js)
-    file.close()
+# def save_dict(dict, filename):
+#     js = json.dumps(dict)
+#     file = open(root_path + filename, 'w')
+#     file.write(js)
+#     file.close()
 
+# classifyDict(AMDict, textDic["AM"])
+# save_dict(AMDict, "AMDict.txt")
+# print(1)
+# classifyDict(LADict, textDic["LA"])
+# save_dict(LADict, "LADict.txt")
+# print(1)
+# classifyDict(PSDict, textDic["PS"])
+# save_dict(PSDict, "PSDict.txt")
+# print(1)
+# classifyDict(CNDict, textDic["CN"])
+# save_dict(CNDict, "CNDict.txt")
+# print(1)
+# classifyDict(COSDict, textDic["COS"])
+# save_dict(COSDict, "COSDict.txt")
+# print(1)
+# classifyDict(CCDict, textDic["CC"])
+# save_dict(CCDict, "CCDict.txt")
 
-save_dict(AMDict, "AMDict.txt")
-save_dict(LADict, "LADict.txt")
-save_dict(PSDict, "PSDict.txt")
-save_dict(CNDict, "CNDict.txt")
-save_dict(COSDict, "COSDict.txt")
-save_dict(CCDict, "CCDict.txt")
-
-for title, list in AMDict.items():
-    print(f"{title}: {len(list)}")
+# for title, list in AMDict.items():
+#     print(f"{title}: {len(list)}")
